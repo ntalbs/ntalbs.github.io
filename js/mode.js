aa931769893da7174dda5abe253d484a7700f5b9
Seongjun Yun <seongjun@amazon.com>
2020-02-14 17:31:01 +0000

    New post: dark-mode

--- js/mode.js
+++ js/mode.js
@@ -1,9 +1,9 @@
-const sun  = '☼'
+const sun  = '☀'
 const moon = '☾'
 const light = 'light'
 const dark  = 'dark'
 
-let be = document.getElementsByTagName("BODY")[0];
+let root = document.documentElement
 let modeSwitch = document.getElementById('mode-switch')
 modeSwitch.style.width = '46px'
 
@@ -13,17 +13,17 @@ modeSwitch.addEventListener('click', (e) => {
 
 switchMode(getMode())
 
-function getMode() {
+function getMode () {
   let mode = localStorage.getItem('currentMode')
   return !mode ? light: mode;
 }
 
-function setMode(mode) {
+function setMode (mode) {
   localStorage.setItem('currentMode', mode)
   modeSwitch.textContent = (mode===light ? moon : sun)
 }
 
-function switchMode(mode) {
+function switchMode (mode) {
   if (mode === light) {
     setMode(light)
     makeLight()
@@ -31,44 +31,49 @@ function switchMode(mode) {
     setMode(dark)
     makeDark()
   }
+  resetDisqus()
 }
 
-function makeDark(){
-  be.style.setProperty('--background', "#2a2e2e");
-  be.style.setProperty('--text', "#bbb");
-  be.style.setProperty('--heading', "#eee");
-  be.style.setProperty('--link', "#aaa");
-  be.style.setProperty('--link-hover', "#ccc");
-  be.style.setProperty('--inline-code-bg', "#444");
-  be.style.setProperty('--code-block-bg', "#888");
-  be.style.setProperty('--box-fg', "#aaa");
-  be.style.setProperty('--box-bg', "#3a3a3a");
+function makeDark () {
+  root.style.setProperty('--background', "#2a2e2e");
+  root.style.setProperty('--text', "#bbb");
+  root.style.setProperty('--heading', "#eee");
+  root.style.setProperty('--link', "#aaa");
+  root.style.setProperty('--link-hover', "#ccc");
+  root.style.setProperty('--inline-code-bg', "#444");
+  root.style.setProperty('--code-block-bg', "#888");
+  root.style.setProperty('--box-fg', "#aaa");
+  root.style.setProperty('--box-bg', "#3a3a3a");
 
-  be.style.setProperty('--archive-ul', "#888");
-  be.style.setProperty('--post-meta', "#666");
-  be.style.setProperty('--tagbox', "#888");
+  root.style.setProperty('--archive-ul', "#888");
+  root.style.setProperty('--post-meta', "#666");
+  root.style.setProperty('--tagbox', "#888");
 
-  be.style.setProperty('--line', "#444");
-  be.style.setProperty('--circle', "#444");
-  be.style.setProperty('--footer', "#333");
+  root.style.setProperty('--line', "#444");
+  root.style.setProperty('--circle', "#444");
+  root.style.setProperty('--footer', "#333");
 }
 
-function makeLight(){
-  be.style.setProperty('--background', "#fff");
-  be.style.setProperty('--text', "");
-  be.style.setProperty('--heading', "");
-  be.style.setProperty('--link', "#666");
-  be.style.setProperty('--link-hover', "#999");
-  be.style.setProperty('--inline-code-bg', "#eee");
-  be.style.setProperty('--code-block-bg', "#eee");
-  be.style.setProperty('--box-fg', "#808080");
-  be.style.setProperty('--box-bg', "#f5f5f5");
+function makeLight () {
+  root.style.setProperty('--background', "#fff");
+  root.style.setProperty('--text', "");
+  root.style.setProperty('--heading', "");
+  root.style.setProperty('--link', "#666");
+  root.style.setProperty('--link-hover', "#999");
+  root.style.setProperty('--inline-code-bg', "#eee");
+  root.style.setProperty('--code-block-bg', "#eee");
+  root.style.setProperty('--box-fg', "#808080");
+  root.style.setProperty('--box-bg', "#f5f5f5");
 
-  be.style.setProperty('--archive-ul', "#444");
-  be.style.setProperty('--post-meta', "#aaa");
-  be.style.setProperty('--tagbox', "#555");
+  root.style.setProperty('--archive-ul', "#444");
+  root.style.setProperty('--post-meta', "#aaa");
+  root.style.setProperty('--tagbox', "#555");
 
-  be.style.setProperty('--line', "#ebf2f6");
-  be.style.setProperty('--circle', "#fff");
-  be.style.setProperty('--footer', "#eee");
+  root.style.setProperty('--line', "#ebf2f6");
+  root.style.setProperty('--circle', "#fff");
+  root.style.setProperty('--footer', "#eee");
+}
+
+function resetDisqus() {
+  DISQUS.reset({reload: true});
 }
